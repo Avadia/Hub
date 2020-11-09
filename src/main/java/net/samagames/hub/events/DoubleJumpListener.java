f9c95f8d0f74f71f723e4fd5272c66101c01ca8d
FarmVivi <farmvividev@gmail.com>
2020-11-09 15:56:24 +0100

    Fix welcome tutorial fall bug

--- src/main/java/net/samagames/hub/events/DoubleJumpListener.java
+++ src/main/java/net/samagames/hub/events/DoubleJumpListener.java
@@ -30,11 +30,10 @@ import java.util.UUID;
  * along with Hub.  If not, see <http://www.gnu.org/licenses/>.
  */
 public class DoubleJumpListener implements Listener {
-    private final List<UUID> allowed;
+    public static final List<UUID> allowed = new ArrayList<>();
     private final Hub hub;
 
     public DoubleJumpListener(Hub hub) {
-        this.allowed = new ArrayList<>();
         this.hub = hub;
     }
 
@@ -51,16 +50,16 @@ public class DoubleJumpListener implements Listener {
 
         if (event.getPlayer().isOnGround()) {
             event.getPlayer().setAllowFlight(true);
-            this.allowed.add(event.getPlayer().getUniqueId());
+            allowed.add(event.getPlayer().getUniqueId());
         }
     }
 
     @EventHandler
     public void onPlayerToggleFlight(PlayerToggleFlightEvent event) {
-        if (!this.allowed.contains(event.getPlayer().getUniqueId()))
+        if (!allowed.contains(event.getPlayer().getUniqueId()))
             return;
 
-        this.allowed.remove(event.getPlayer().getUniqueId());
+        allowed.remove(event.getPlayer().getUniqueId());
 
         if (this.hub.getPlayerManager().isBusy(event.getPlayer()))
             return;
